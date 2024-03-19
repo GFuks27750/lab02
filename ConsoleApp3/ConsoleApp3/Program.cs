bc71829b7eb1fe7c6e677a692c70223761654dec
GFuks27750 <s27750@pjwstk.edu.pl>
2024-03-19 22:25:22 +0100

    Dodalem metode zwracajaca maksymalna wartosc z tablicy

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static double Max(int[] array)
+    {
+        double max = Queryable.Max(array.AsQueryable());
+        return max; 
+    }
+
+    static void Main()
+    {
+        var arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        Console.WriteLine(Max(arr));
+    }
+}
