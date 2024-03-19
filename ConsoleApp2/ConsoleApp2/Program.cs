4eb78873cd963935f003b8e3f6846702e6c7df53
GFuks27750 <s27750@pjwstk.edu.pl>
2024-03-19 22:01:16 +0100

    Sprawdzilem metode Average

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,8 +1,16 @@
 ﻿class Program
 {
-    public static double Average(int[] array)
+    
+    static double Average(int[] array)
     {
            double average =  Queryable.Average(array.AsQueryable());
            return average;
     }
+
+    static void Main()
+    {
+        var arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        Console.WriteLine(Average(arr));
+    }
+    
 }
--- ConsoleApp2/ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49db81cfe7c7084160a21aa2c8995a1859357253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08703544877b0fccd32d9015e0c7f417f10a9e04")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
